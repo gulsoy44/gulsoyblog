8bbf6646ea61e9f4c504eb1d96bb352ce5d29807
gulsoy44 <gul.soy@outlook.com>
2025-07-26 07:57:24 +0300

    v4

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -1,16 +1,18 @@
-import os
-from pathlib import Path
 
-import dj_database_url
-
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
-#DEBUG = True # Set to False in production
 
-DEBUG = os.environ.get("DEBUG","False").lower() == "true"
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-t+y17c7s6784yzxf%gthqoowd*p2(hj0f-&$l%v8%kfh0x4nm^'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -31,12 +33,20 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'MyBlog.urls'
 
 
-#postgresql://djangodb_96ah_user:vRrokrD36wqO2Lry2O1rjGiWCzKygepc@dpg-d21qvjqdbo4c73ehelfg-a.oregon-postgres.render.com/djangodb_96ah
+
 
 TEMPLATES = [
     {
@@ -55,17 +65,16 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'MyBlog.wsgi.application'
+import dj_database_url
+import os
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True  # Ensure SSL connection on Render
+    )
 }
-
-database_url = os.environ.get("DATABASE_URL")
-
-DATABASES ["default"]=dj_database_url.parse(database_url)
 
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -91,13 +100,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
